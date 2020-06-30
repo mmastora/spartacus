f933d054be2ba2d68e4c89ebb9cd5de3ac49152c
Nikola Zarić <nikola.zaric.ns@gmail.com>
2020-06-30 20:21:41 +0200

    component refactor

--- projects/storefrontlib/src/cms-components/configurator/commons/config-overview-loading/config-overview-loading.component.ts
+++ projects/storefrontlib/src/cms-components/configurator/commons/config-overview-loading/config-overview-loading.component.ts
@@ -1,44 +1,49 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component } from '@angular/core';
 import {
   Configurator,
   ConfiguratorCommonsService,
   RoutingService,
 } from '@spartacus/core';
+import { Observable, using } from 'rxjs';
+import { switchMap, tap } from 'rxjs/operators';
+import { BREAKPOINT } from '../../../../layout';
+import { BreakpointService } from '../../../../layout/breakpoint/breakpoint.service';
 import { ConfigurationRouter } from '../../generic/service/config-router-data';
 import { ConfigRouterExtractorService } from '../../generic/service/config-router-extractor.service';
-import { switchMap } from 'rxjs/operators';
-import { BreakpointService } from '../../../../layout/breakpoint/breakpoint.service';
-import { BREAKPOINT } from '../../../../layout';
 
 @Component({
   selector: 'cx-config-overview-loading',
   templateUrl: './config-overview-loading.component.html',
   styles: [],
 })
-export class ConfigOverviewLoadingComponent implements OnInit {
-  routerData$: Observable<ConfigurationRouter.Data>;
-  breakpoint$: Observable<BREAKPOINT>;
-  configuration$: Observable<Configurator.Configuration>;
+export class ConfigOverviewLoadingComponent {
+  breakpoint: BREAKPOINT;
+
+  protected routerData$: Observable<
+    ConfigurationRouter.Data
+  > = this.configRouterExtractorService.extractRouterData(this.routingService);
+  protected breakpoint$: Observable<
+    BREAKPOINT
+  > = this.breakpointService.breakpoint$.pipe(
+    tap((breakpoint) => (this.breakpoint = breakpoint))
+    // TODO: instead of the breakpoint, we could load an appropriate SVG here,
+    // and set it as a property of the component.
+  );
+
+  configuration$: Observable<Configurator.Configuration> = using(
+    () => this.breakpoint$.subscribe(),
+    () =>
+      this.routerData$.pipe(
+        switchMap((routerData) =>
+          this.configuratorCommonsService.getConfiguration(routerData.owner)
+        )
+      )
+  );
 
   constructor(
-    private routingService: RoutingService,
-    private configRouterExtractorService: ConfigRouterExtractorService,
-    private configuratorCommonsService: ConfiguratorCommonsService,
-    private breakpointService: BreakpointService
+    protected routingService: RoutingService,
+    protected configRouterExtractorService: ConfigRouterExtractorService,
+    protected configuratorCommonsService: ConfiguratorCommonsService,
+    protected breakpointService: BreakpointService
   ) {}
-
-  ngOnInit(): void {
-    this.breakpoint$ = this.breakpointService.breakpoint$;
-
-    this.routerData$ = this.configRouterExtractorService.extractRouterData(
-      this.routingService
-    );
-
-    this.configuration$ = this.routerData$.pipe(
-      switchMap((routerData) =>
-        this.configuratorCommonsService.getConfiguration(routerData.owner)
-      )
-    );
-  }
 }
