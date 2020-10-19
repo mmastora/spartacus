3062a4bdb8820e7ca5c52565118f74309e7d66dd
Thambirajah <renuchan.thambirajah@sap.com>
2020-10-19 15:32:48 -0400

    Fix: Made fixes for issue #9037. Added corresponding unit tests.

--- projects/storefrontlib/src/cms-components/checkout/guards/checkout-auth.guard.ts
+++ projects/storefrontlib/src/cms-components/checkout/guards/checkout-auth.guard.ts
@@ -14,7 +14,7 @@ import {
   B2BUserGroup,
 } from '@spartacus/core';
 import { combineLatest, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, filter } from 'rxjs/operators';
 import { CheckoutConfigService } from '../services/checkout-config.service';
 
 @Injectable({
@@ -36,32 +36,41 @@ export class CheckoutAuthGuard implements CanActivate {
       this.authService.getUserToken(),
       this.activeCartService.getAssignedUser(),
       this.userService.get(),
+      this.activeCartService.isStable(),
     ]).pipe(
-      map(([token, cartUser, user]: [UserToken, User, User | B2BUser]) => {
-        if (!token.access_token) {
-          if (this.activeCartService.isGuestCart()) {
-            return Boolean(cartUser);
+      filter(([, , , isStable]) => !!isStable),
+      map(
+        ([token, cartUser, user]: [
+          UserToken,
+          User,
+          User | B2BUser,
+          boolean
+        ]) => {
+          if (!token.access_token) {
+            if (this.activeCartService.isGuestCart()) {
+              return Boolean(cartUser);
+            }
+            this.authRedirectService.reportAuthGuard();
+            if (this.checkoutConfigService.isGuestCheckout()) {
+              this.routingService.go({ cxRoute: 'login' }, { forced: true });
+            } else {
+              this.routingService.go({ cxRoute: 'login' });
+            }
+          } else if ('roles' in user) {
+            const roles = (<B2BUser>user).roles;
+            if (roles.includes(B2BUserGroup.B2B_CUSTOMER_GROUP)) {
+              return true;
+            } else {
+              this.globalMessageService.add(
+                { key: 'checkout.invalid.accountType' },
+                GlobalMessageType.MSG_TYPE_WARNING
+              );
+              return false;
+            }
           }
-          if (this.checkoutConfigService.isGuestCheckout()) {
-            this.routingService.go({ cxRoute: 'login' }, { forced: true });
-          } else {
-            this.routingService.go({ cxRoute: 'login' });
-          }
-          this.authRedirectService.reportAuthGuard();
-        } else if ('roles' in user) {
-          const roles = (<B2BUser>user).roles;
-          if (roles.includes(B2BUserGroup.B2B_CUSTOMER_GROUP)) {
-            return true;
-          } else {
-            this.globalMessageService.add(
-              { key: 'checkout.invalid.accountType' },
-              GlobalMessageType.MSG_TYPE_WARNING
-            );
-            return false;
-          }
+          return !!token.access_token;
         }
-        return !!token.access_token;
-      })
+      )
     );
   }
 }
